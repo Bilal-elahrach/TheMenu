2a92c35bb1ece140c405afd78c05777d2e1cb127
Bilal El <elahrachbilal@gmail.com>
2025-05-03 23:54:27 +0100

    Added Node struct and function prototypes to linked_list_1.h

---
+++ linked_list_1.h
@@ -0,0 +1,15 @@
+#ifndef LINKED_LIST_1_H
+#define LINKED_LIST_1_H
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+}Node;
+
+Node* createNode(int data);
+void insertNode(Node** head, int data);
+void printList(Node* head);
+void insertNodeEnd(Node** head, int data);
+void deleteNode(Node** head, int data);
+int countNodes(Node* head);
+#endif
