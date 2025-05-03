e05407dff2622832ee0018de0d8441d9a31e5c37
Bilal El <elahrachbilal@gmail.com>
2025-05-03 23:53:11 +0100

    Updated linked_list_1.c to include linked_list_1.h and removed main

--- linked_list_1.c
+++ linked_list_1.c
@@ -3,12 +3,9 @@
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
+#include "linked_list_1.h"
 
 
-typedef struct Node {
-    int data;
-    struct Node* next;
-}Node;
 
 Node* createNode(int data){
     Node* new; //Declares a pointer to a Node struc that we name new
@@ -110,44 +107,4 @@ int countNodes(Node* head){
     }
 
     return count;
-}
-
-
-int main () {
-    Node* head = NULL;
-    Node* tail = NULL;
-    int value;
-    int count;
-
-    do{
-        printf("Enter Value of the new Node or 0 to display the result: ");
-        scanf("%d", &value);
-        if(value != 0){
-            insertNode(&head, value);
-            insertNodeEnd(&tail, value);
-            continue;
-        };
-        break;
-    }while(1);
-
-    if(value == 0){
-        printf("\nPrepend: ");
-        printList(head);
-        
-        printf("Append: ");
-        printList(tail);
-
-    }
-
-    printf("\nNow, we are going to delete a node from Prepend.\n\nNode to delete: ");
-    scanf("%d", &value);
-    printf("Original Prepend: ");
-    printList(head);
-    deleteNode(&head, value);
-    printf("\nNew Prepend: ");
-    printList(head);
-
-    printf("\nNext, we count the nodes in Prepend.\nThat would be: %d Nodes", count = countNodes(head));
-    
-    return 0;
 }
